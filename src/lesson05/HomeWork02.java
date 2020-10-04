18b41d5106e70a40c7f54ed863012f6ec9458a1d
vladkirov <vladkirov@gmail.com>
2020-10-04 16:23:21 +0300

    2nd task

--- src/lesson05/HomeWork02.java
+++ src/lesson05/HomeWork02.java
@@ -2,7 +2,7 @@ package lesson05;
 
 public class HomeWork02 {
     public static void main(String[] args) {
-        String inputString = "дом домик домой одомашненный";
+        String inputString = "дом домик домой одомашненный дом";
         String Pattern = "дом";
 
         System.out.println(inputString.split(Pattern).length - 1);
