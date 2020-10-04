dd9a2469e3ab07102a8132d5e4bb3552c07b1e7d
vladkirov <vladkirov@gmail.com>
2020-10-04 20:33:05 +0300

    Names.

--- src/lesson05/HomeWork03.java
+++ src/lesson05/HomeWork03.java
@@ -3,15 +3,15 @@ package lesson05;
 public class HomeWork03 {
     public static void main(String[] args) {
         String originalString = "re t 02 0ter";
-        boolean isPalindrom = true;
+        boolean isPalindrome = true;
 
         originalString = originalString.replace(" ", "").toLowerCase();
         for (int i = 0; i < (originalString.length() / 2); i++)
             if (originalString.charAt(i) != originalString.charAt(originalString.length() - i - 1)) {
-                isPalindrom = false;
+                isPalindrome = false;
                 break;
             }
 
-        System.out.println("Is palindrom: " + isPalindrom);
+        System.out.println("Is palindrome: " + isPalindrome);
     }
 }
