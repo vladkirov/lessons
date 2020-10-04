3083ae7fb839d008a3a57c12c57de06c4750f71e
vladkirov <vladkirov@gmail.com>
2020-10-04 20:31:22 +0300

    No spaces and lowerCase.

--- src/lesson05/HomeWork03.java
+++ src/lesson05/HomeWork03.java
@@ -5,7 +5,7 @@ public class HomeWork03 {
         String originalString = "re t 02 0ter";
         boolean isPalindrom = true;
 
-        originalString = originalString.replace(" ", "");
+        originalString = originalString.replace(" ", "").toLowerCase();
         for (int i = 0; i < (originalString.length() / 2); i++)
             if (originalString.charAt(i) != originalString.charAt(originalString.length() - i - 1)) {
                 isPalindrom = false;
