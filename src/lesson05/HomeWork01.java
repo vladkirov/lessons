a19ceaa53d1e908fe169035e978664416bb437dd
vladkirov <vladkirov@gmail.com>
2020-10-04 15:49:00 +0300

    First task

---
+++ src/lesson05/HomeWork01.java
@@ -0,0 +1,38 @@
+package lesson05;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class HomeWork01 {
+    public static void main(String[] args) {
+        System.out.print("Input amount elements of array: ");
+        Scanner scanner = new Scanner(System.in);
+        int amountElements = scanner.nextInt();
+        scanner.nextLine();
+
+        String[] words = new String[amountElements];
+
+        int addedElements = 0;
+        final String exitWord = "exit";
+
+        System.out.println("Now input any words or <exit> for stop working.");
+        while (amountElements != addedElements) {
+            System.out.println("Input word:");
+            String inputWord = scanner.nextLine();
+            if (inputWord.equals(exitWord)) {
+                break;
+            } else {
+                boolean elementExist = false;
+                for (int i = 0; i < addedElements; i++) {
+                    if (words[i].equals(inputWord)) {
+                        elementExist = true;
+                        break;
+                    }
+                }
+                if (!elementExist) words[addedElements++] = inputWord;
+            }
+        }
+        System.out.println("Array with unique elements:");
+        System.out.println(Arrays.deepToString(words));
+    }
+}
