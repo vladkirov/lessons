6e019a873d98f931b73124eab1dea320f35d22d6
vladkirov <vladkirov@gmail.com>
2020-10-04 16:45:23 +0300

    4d task

---
+++ src/lesson05/HomeWork04.java
@@ -0,0 +1,10 @@
+package lesson05;
+
+public class HomeWork04 {
+    public static void main(String[] args) {
+        String sample = "heLlo";
+
+        sample = sample.toLowerCase();
+        System.out.println(sample.substring(0,1).toUpperCase() + sample.substring(1));
+    }
+}
